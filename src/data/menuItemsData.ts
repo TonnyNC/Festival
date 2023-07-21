67365278254f4ed7bce6a8f444048c8ff9e9c2f6
Tonny Nerso Cayo Villena <tony.nerso@gmail.com>
2023-07-21 18:27:02 -0500

    update components and assets

---
+++ src/data/menuItemsData.ts
@@ -0,0 +1,18 @@
+import { MenuItem } from "../types/types";
+
+const menuItems : MenuItem[] = [
+    {
+        title : 'Home',
+        url : '/'
+    },
+    {
+        title : 'Gremios',
+        url : '/gremios'
+    },
+    {
+        title : 'Cronogama',
+        url : '/cronograma'
+    }
+];
+
+export default menuItems;
